f087cf1442a6f8c0165119e51e4a9ed58c7c2b3d
cfinnegn <cfinnegn@udel.edu>
2017-05-19 14:24:45 -0400

    fixed button codes and backspacing

---
+++ Assets/JoyTypeRegions.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoyTypeRegions : MonoBehaviour {
+
+	public bool isLeft;
+	public bool inputGiven;
+
+	public Text[] regions = new Text[10];
+	public int active_region = 0;
+
+	public float x;
+	public float y;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(isLeft) {
+			x = (float)Math.Round(Input.GetAxis("LSX"), 2);
+			y = (float)Math.Round(Input.GetAxis("LSY"), 2);
+		}
+		else {
+			x = (float)Math.Round(Input.GetAxis("RSX"), 2);
+			y = (float)Math.Round(Input.GetAxis("RSY"), 2);
+		}
+
+		if(y == 1) {    // up
+			active_region = 2;
+		}
+		else if(y == -1) {  // down
+			active_region = 8;
+		}
+		else if(x == 1) {   // right
+			active_region = 6;
+		}
+		else if(x == -1) {  // left
+			active_region = 4;
+		}
+		else if(y > 0.3) {  // diag up
+			if(x > 0.3) //right
+				active_region = 3;
+			else if(x < -0.3)   // left
+				active_region = 1;
+			else
+				active_region = 0;
+		}
+		else if(y < -0.3) {  // diag down
+			if(x > 0.3) //right
+				active_region = 9;
+			else if(x < -0.3)   // left
+				active_region = 7;
+			else
+				active_region = 0;
+		}
+		else {
+			active_region = 0;
+		}
+		if(active_region == 0)
+			inputGiven = false;
+
+		int i = 0;
+		while(i < regions.Length) {
+			if(regions[i] != null) {
+				if(i == active_region)
+					regions[i].color = Color.green;
+				else
+					regions[i].color = Color.white;
+			}
+			i++;
+		}
+	}
+}
---
+++ Assets/JoyTypeStick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoyTypeStick : MonoBehaviour {
+
+	public bool isLeft;
+	public float x;
+	public float y;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isLeft)
+			transform.position = new Vector3(Input.GetAxis("LSX")-4, Input.GetAxis("LSY")-2, 0);
+		else
+			transform.position = new Vector3(Input.GetAxis("RSX")+4, Input.GetAxis("RSY")-2, 0);
+
+		x = Input.GetAxis("LSX");
+		y = Input.GetAxis("LSY");
+	}
+}
+  //movementVector.x = Input.GetAxis("LeftJoystickX") * movementSpeed;
+  //movementVector.z = Input.GetAxis("LeftJoystickY") * movementSpeed;
